a1307225fdfa6a896c1e385caafe234d05101a95
Bram Peirs <bram.peirs@showpad.com>
2023-06-05 14:44:56 +0200

    add FE caching to overview
    (show prev results while fetching new results)

--- src/app/containers/overview/overview.component.ts
+++ src/app/containers/overview/overview.component.ts
@@ -4,6 +4,7 @@ import { ButtonComponent } from 'src/app/components/button/button.component';
 import { CardComponent } from 'src/app/components/card/card.component';
 import { RouterLink } from '@angular/router';
 import { PollService } from 'src/app/services/poll.service';
+import { Poll } from 'src/app/services/poll.model';
 
 @Component({
   selector: 'bp-overview',
@@ -16,5 +17,13 @@ export class OverviewComponent {
   date = new Date();
 
   private pollService = inject(PollService);
-  public polls$ = this.pollService.getpolls();
+  public polls$ = this.pollService.polls$;
+
+  constructor() {
+    this.pollService.loadPolls();
+  }
+
+  trackById(index: number, poll: Poll): string {
+    return poll.id;
+  }
 }
--- src/app/services/poll.service.ts
+++ src/app/services/poll.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 import { Poll, PollPostDto } from './poll.model';
 import { HttpClient } from '@angular/common/http';
 import { PollResponse } from './poll-response.model';
@@ -10,11 +10,17 @@ import { PollResponse } from './poll-response.model';
 export class PollService {
   private readonly apiUrl = 'https://api.babypeirs.be';
   private httpClient = inject(HttpClient);
+  private readonly _polls$ = new BehaviorSubject<Poll[]>([]);
 
-  getpolls(): Observable<Poll[]> {
-    return this.httpClient
+  public readonly polls$ = this._polls$.asObservable();
+
+  loadPolls(): void {
+    this.httpClient
       .get<PollResponse<Poll>>(this.apiUrl)
-      .pipe(map((response) => response.items));
+      .pipe(map((response) => response.items))
+      .subscribe((polls) => {
+        this._polls$.next(polls);
+      });
   }
 
   postPoll(poll: PollPostDto): Observable<void> {
