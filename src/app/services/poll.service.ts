98a6e461ce0432accb8fa85b3164a4ca64236aed
Bram Peirs <bram.peirs@showpad.com>
2023-06-06 11:47:15 +0200

    add isRequesting to service

--- src/app/services/poll.service.ts
+++ src/app/services/poll.service.ts
@@ -11,19 +11,30 @@ export class PollService {
   private readonly apiUrl = 'https://api.babypeirs.be';
   private httpClient = inject(HttpClient);
   private readonly _polls$ = new BehaviorSubject<Poll[]>([]);
+  private readonly _isRequesting$ = new BehaviorSubject<boolean>(false);
 
   public readonly polls$ = this._polls$.asObservable();
+  public readonly isRequesting$ = this._isRequesting$.asObservable();
 
   loadPolls(): void {
+    // don't set requesting if we can use cache
+    if (this.noCacheAvailable()) {
+      this._isRequesting$.next(true);
+    }
     this.httpClient
       .get<PollResponse<Poll>>(this.apiUrl)
       .pipe(map((response) => response.items))
       .subscribe((polls) => {
+        this._isRequesting$.next(false);
         this._polls$.next(polls);
       });
   }
 
   postPoll(poll: PollPostDto): Observable<void> {
     return this.httpClient.post<void>(this.apiUrl, poll);
+  }
+
+  private noCacheAvailable(): boolean {
+    return this._polls$.value?.length === 0;
   }
 }
